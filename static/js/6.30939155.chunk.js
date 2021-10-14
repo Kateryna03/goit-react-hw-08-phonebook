b9d042956990d6acc48b82bb50777bd8710dfff7
Kateryna Gurienkova <gurienkova@gmail.com>
2021-10-14 15:38:02 +0300

    Updates

--- static/js/6.30939155.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[6],{88:function(t,o,e){"use strict";e.r(o);e(0);var n=e(1);o.default=function(){return Object(n.jsx)(n.Fragment,{children:Object(n.jsx)("h1",{children:"This is Start Page of Your ContactsBook"})})}}}]);
-//# sourceMappingURL=6.30939155.chunk.js.map
---
+++ static/js/8.cef8d4f2.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[8],{223:function(t,o,e){"use strict";e.r(o);e(0);var n=e(2);o.default=function(){return Object(n.jsx)(n.Fragment,{children:Object(n.jsx)("h1",{children:"This is Start Page of Your ContactsBook"})})}}}]);
+//# sourceMappingURL=8.cef8d4f2.chunk.js.map
